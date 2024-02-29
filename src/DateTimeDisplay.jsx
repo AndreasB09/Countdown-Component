73367c9911d31b274dee52210d7ae1e84f5f9c19
andreasb09 <andreasb09.kodehode@gmail.com>
2024-02-29 12:25:54 +0100

    cleanup

--- src/DateTimeDisplay.jsx
+++ src/DateTimeDisplay.jsx
@@ -17,3 +17,27 @@ DateTimeDisplay.propTypes = {
 };
 
 export default DateTimeDisplay;
+
+//along with this in contdown timer
+// const DisplayCounter = ({ days, hours, minutes, seconds }) => {
+
+//   return (
+//     <div className="display-counter">
+//       <DateTimeDisplay value={days} type={"Days"} isDanger={days} />
+//       <p>:</p>
+//       <DateTimeDisplay value={hours} type={"Hours"} isDanger={hours} />
+//       <p>:</p>
+//       <DateTimeDisplay value={minutes} type={"Minutes"} isDanger={minutes} />
+//       <p>:</p>
+//       <DateTimeDisplay value={seconds} type={"Seconds"} isDanger={seconds} />
+//       <p>:</p>
+//     </div>
+//   );
+// };
+
+// DisplayCounter.propTypes = {
+//   days: PropTypes.number.isRequired,
+//   hours: PropTypes.number.isRequired,
+//   minutes: PropTypes.number.isRequired,
+//   seconds: PropTypes.number.isRequired,
+// };
--- src/countdownTimer.jsx
+++ src/countdownTimer.jsx
@@ -1,55 +1,37 @@
 // import React from "react";
 import { useCountdown } from "./hooks/useCountdown";
-import DateTimeDisplay from "./DateTimeDisplay";
 import PropTypes from "prop-types";
-import styles from "./CountdownTimer.module.css"
 
 const CountdownTimer = ({ targetDate }) => {
   const [days, hours, minutes, seconds] = useCountdown(targetDate);
 
-  if (days + hours + minutes + seconds <= 0) {
-    return <TimeoutNotice />;
-  } else {
-    return (
-      <ShowCounter
-        days={days}
-        hours={hours}
-        minutes={minutes}
-        seconds={seconds}
-      />
-    );
+  if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
+    return null;
   }
-};
 
-const TimeoutNotice = () => {
   return (
-    <div className="timeout-notice">
-      <span>Timeout</span>
-      <p>Select a time and date</p>
+    <div className="display-counter">
+      <div className={days <= 0 ? "countdown ending" : "countdown"}>
+        <p>{days}</p>
+        <span>Days</span>
+      </div>
+      <p>:</p>
+      <div className={hours <= 0 ? "countdown ending" : "countdown"}>
+        <p>{hours}</p>
+        <span>Hours</span>
+      </div>
+      <p>:</p>
+      <div className={minutes <= 0 ? "countdown ending" : "countdown"}>
+        <p>{minutes}</p>
+        <span>Minutes</span>
+      </div>
+      <p>:</p>
+      <div className={seconds <= 0 ? "countdown ending" : "countdown"}>
+        <p>{seconds}</p>
+        <span>Seconds</span>
+      </div>
     </div>
   );
-};
-
-const ShowCounter = ({ days, hours, minutes, seconds }) => {
-  return (
-    <div className={styles.showcounter}>
-      <DateTimeDisplay value={days} type={"Days"} isDanger={days} />
-      <p>:</p>
-      <DateTimeDisplay value={hours} type={"Hours"} isDanger={hours} />
-      <p>:</p>
-      <DateTimeDisplay value={minutes} type={"Minutes"} isDanger={minutes} />
-      <p>:</p>
-      <DateTimeDisplay value={seconds} type={"Seconds"} isDanger={seconds} />
-      <p>:</p>
-    </div>
-  );
-};
-
-ShowCounter.propTypes = {
-  days: PropTypes.number.isRequired,
-  hours: PropTypes.number.isRequired,
-  minutes: PropTypes.number.isRequired,
-  seconds: PropTypes.number.isRequired,
 };
 
 CountdownTimer.propTypes = {
