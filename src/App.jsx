0fae60be886838ae5c5c03be3ef1b7d416846030
AndreasB09 <andreasb09.kodehode@gmail.com>
2024-03-05 14:19:54 +0100

    font

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ export default function App() {
   const targetDate = new Date("2024-03-28T14:30:00");
 
   return (
-    <div>
-      <h1>Hey Universe</h1>
+    <div className='container'>
+      <h1 className='title'>Countdown to Swedish Adventures</h1>
       <CountdownTimer targetDate={targetDate.getTime()} />
     </div>
   );
